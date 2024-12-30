01e82e0b9699cb3253edcf268358f92e6b766c1d
leeningzzu <leeningzzu@gmail.com>
2024-12-30 08:13:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import requests
 import os
 import random
-from bs4 import BeautifulSoup
 
 today = datetime.now()
 start_date = os.environ['START_DATE']
